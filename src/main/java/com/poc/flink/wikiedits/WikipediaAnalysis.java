933e878eb30be5cae9d2edeaccf0d3032323f134
praneel <praneelgavali@gmail.com>
2018-12-26 16:12:46 +0530

    POC of Streaming Analysis job. It analysis wikipedia edits with username as key.

---
+++ src/main/java/com/poc/flink/wikiedits/WikipediaAnalysis.java
@@ -0,0 +1,50 @@
+package com.poc.flink.wikiedits;
+
+import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
+import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
+
+
+/**
+ * Created by praneel on 26 Dec, 2018
+ */
+public class WikipediaAnalysis {
+
+    public static void main(String[] args) throws Exception {
+
+        //The first step in a Flink program is to create a StreamExecutionEnvironment (or ExecutionEnvironment if you are writing a batch job).
+        // This can be used to set execution parameters and create sources for reading from external systems
+        StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
+
+        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
+                .keyBy(new KeySelector<WikipediaEditEvent, String>() {
+                    @Override
+                    public String getKey(WikipediaEditEvent event) {
+                        return event.getUser();
+                    }
+                });
+
+        DataStream<Tuple2<String, Long>> result = keyedEdits
+                .timeWindow(Time.seconds(1))
+                .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
+                        acc.f0 = event.getUser();
+                        acc.f1 += event.getByteDiff();
+                        return acc;
+                    }
+                });
+        result.print();
+
+        see.execute();
+
+    }
+}
